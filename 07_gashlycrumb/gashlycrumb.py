df709708afbe80ceada701d7bc042241f1ca4c0d
Trevor Freeman <trevor.freeman@utexas.edu>
2021-11-08 10:25:05 -0600

    setup args

--- 07_gashlycrumb/gashlycrumb.py
+++ 07_gashlycrumb/gashlycrumb.py
@@ -2,7 +2,7 @@
 """
 Author : treevooor <treevooor@localhost>
 Date   : 2021-11-08
-Purpose: Rock the Casbah
+Purpose: Gashlycrumb
 """
 
 import argparse
@@ -13,38 +13,21 @@ def get_args():
     """Get command-line arguments"""
 
     parser = argparse.ArgumentParser(
-        description='Rock the Casbah',
+        description='Gashlycrumb',
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
-    parser.add_argument('positional',
-                        metavar='str',
-                        help='A positional argument')
-
-    parser.add_argument('-a',
-                        '--arg',
-                        help='A named string argument',
-                        metavar='str',
-                        type=str,
-                        default='')
-
-    parser.add_argument('-i',
-                        '--int',
-                        help='A named integer argument',
-                        metavar='int',
-                        type=int,
-                        default=0)
+    parser.add_argument('letter',
+                        metavar='letter',
+                        help='Letter(s)',
+                        nargs='+')
 
     parser.add_argument('-f',
                         '--file',
-                        help='A readable file',
+                        help='Input file',
                         metavar='FILE',
                         type=argparse.FileType('rt'),
-                        default=None)
+                        default='gashlycrumb.txt')
 
-    parser.add_argument('-o',
-                        '--on',
-                        help='A boolean flag',
-                        action='store_true')
 
     return parser.parse_args()
 
@@ -54,16 +37,10 @@ def main():
     """Make a jazz noise here"""
 
     args = get_args()
-    str_arg = args.arg
-    int_arg = args.int
     file_arg = args.file
-    flag_arg = args.on
     pos_arg = args.positional
 
-    print(f'str_arg = "{str_arg}"')
-    print(f'int_arg = "{int_arg}"')
     print('file_arg = "{}"'.format(file_arg.name if file_arg else ''))
-    print(f'flag_arg = "{flag_arg}"')
     print(f'positional = "{pos_arg}"')
 
 
