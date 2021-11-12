4f009a2b0f8ac241a6819c580c6e6f217d4fc9b6
Trevor Freeman <trevor.freeman@utexas.edu>
2021-11-11 21:39:10 -0600

    setup args

--- 09_abuse/abuse.py
+++ 09_abuse/abuse.py
@@ -2,7 +2,7 @@
 """
 Author : treevooor <treevooor@localhost>
 Date   : 2021-11-11
-Purpose: Rock the Casbah
+Purpose: Heap abuse 
 """
 
 import argparse
@@ -13,38 +13,29 @@ def get_args():
     """Get command-line arguments"""
 
     parser = argparse.ArgumentParser(
-        description='Rock the Casbah',
+        description='Heap abuse',
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
-    parser.add_argument('positional',
-                        metavar='str',
-                        help='A positional argument')
-
     parser.add_argument('-a',
-                        '--arg',
-                        help='A named string argument',
-                        metavar='str',
-                        type=str,
-                        default='')
-
-    parser.add_argument('-i',
-                        '--int',
-                        help='A named integer argument',
-                        metavar='int',
+                        '--adjectives',
+                        help='Number of adjectives',
+                        metavar='adjectives',
                         type=int,
-                        default=0)
+                        default=2)
 
-    parser.add_argument('-f',
-                        '--file',
-                        help='A readable file',
-                        metavar='FILE',
-                        type=argparse.FileType('rt'),
+    parser.add_argument('-n',
+                        '--number',
+                        help='Number of insults',
+                        metavar='insults',
+                        type=int,
+                        default=3)
+
+    parser.add_argument('-s',
+                        '--seed',
+                        help='Random seed',
+                        metavar='seed',
+                        type=int,
                         default=None)
-
-    parser.add_argument('-o',
-                        '--on',
-                        help='A boolean flag',
-                        action='store_true')
 
     return parser.parse_args()
 
@@ -54,17 +45,6 @@ def main():
     """Make a jazz noise here"""
 
     args = get_args()
-    str_arg = args.arg
-    int_arg = args.int
-    file_arg = args.file
-    flag_arg = args.on
-    pos_arg = args.positional
-
-    print(f'str_arg = "{str_arg}"')
-    print(f'int_arg = "{int_arg}"')
-    print('file_arg = "{}"'.format(file_arg.name if file_arg else ''))
-    print(f'flag_arg = "{flag_arg}"')
-    print(f'positional = "{pos_arg}"')
 
 
 # --------------------------------------------------
