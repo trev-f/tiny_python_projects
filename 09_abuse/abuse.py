7ca699f3615300886a718a22957195ce9435d707
Trevor Freeman <trevor.freeman@utexas.edu>
2021-11-14 18:14:09 -0600

    add adj and noun lists for insults

--- 09_abuse/abuse.py
+++ 09_abuse/abuse.py
@@ -53,6 +53,24 @@ def main():
 
     args = get_args()
 
+    adjs = """bankrupt base caterwauling corrupt cullionly detestable 
+              dishonest false filthsome filthy foolish foul gross 
+              heedless indistinguishable infected insatiate irksome
+              lascivious lecherous loathsome lubbery old peevish rascaly
+              rotten ruinous scurilous scurvy slanderous sodden-witted 
+              thin-faced toad-spotted unmannered vile wall-eyed
+           """.split()
+
+    nouns = """Judas Satan ape ass barbermonger beggar block boy
+               braggart butt carbuncle coward coxcomb cur dandy 
+               degenerate fiend fishmonger fool gull harpy jack jolthead 
+               knave liar lunatic maw milksop minion ratcatcher recreant 
+               rogue scold slave swine traitor varlet villain worm
+            """.split()
+
+    print(adjs)
+    print(nouns)
+
 
 # --------------------------------------------------
 if __name__ == '__main__':
